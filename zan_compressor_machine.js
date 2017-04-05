0edf7a31504ea02606e80f23735fb167d41a0425
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-04-05 13:49:13 +0200

    stats, more easily 'relocable' program code in 'tape'(relative offset from current instruction position)

--- zan_compressor_machine.js
+++ zan_compressor_machine.js
@@ -14,9 +14,9 @@ that can be obtained again by running the program.
 function prog_test_1(){
 	const dst=3 // 3->out
 	const program0=[dst,1,1,1]
-	const program1=[dst,0,2,2]
-	const program2=[dst,1,3,3]
-	const program3=[dst,0,3,3]
+	const program1=[dst,0,1,1]
+	const program2=[dst,1,1,1]
+	const program3=[dst,0,1,1]
 	const program=[program0,program1,program2,program3]
 
 	return program
@@ -24,12 +24,12 @@ function prog_test_1(){
 function prog_test_1_opt(){
 	const dst=3 // 3->out
 	const program0=[dst,1,1,1]
-	const program1=[dst,0,0,0]
+	const program1=[dst,0,-1,-1]
 	const program=[program0,program1]
 
 	return program
 }
-const program=prog_test_1_opt()
+const program=prog_test_1()
 
 const out_expected_array = [1,0,1,0]
 const out_expected_ref_len = out_expected_array.length // it is saved as it will vary
@@ -59,13 +59,14 @@ set program(program_to_set){
 	
 	this.program_array=program_to_set
 
-	this.current_instruction_index = this.min_instruction_index
-	this.out_expected_working = out_expected_array
+	this.current_instruction_index=this.min_instruction_index
+	this.out_expected_working=out_expected_array
 
 	// counters
 	this.no_out_counter=0
 	this.out_counter=0
 	this.step_counter=0
+	this.update_counter=new Array(this.program_array.length).fill(0)
 }
 
 // execution loop
@@ -74,54 +75,58 @@ step(){
 	this.step_counter++
 
 	// fetch current intruction
-	const current_instruction = this.program_array[this.current_instruction_index]
+	const current_instruction=this.program_array[this.current_instruction_index]
 
 	// ---------- bit-copy operation -----------
 
 	// 2nd field is for copy-origin
-	const field_origin = current_instruction[1]
+	const field_origin=current_instruction[1]
 
 	// read data from memory
-	const bit_data = this.memory[field_origin]
-
+	var bit_data
+	if(field_origin==0||field_origin==1)bit_data=field_origin
+	else bit_data=this.memory[field_origin+this.current_instruction_index]
 
 	// write the data that was read
 
 	// 1st field is copy-destination
-	const field_destination = current_instruction[0]
-
+	const field_destination=current_instruction[0]
 
 	const correct_output=bit_data==this.out_expected_working[0]
 
 	//DESTINATION_OUTPUT_PORT
-	if ( field_destination == 3 ) {
+	if(field_destination==3){
 
-		if(correct_output) {
+		if(correct_output){
 			this.out_expected_working=this.out_expected_working.slice(1)
 			console.log( bit_data )
 			this.out_counter++
 			this.no_out_counter=0
+			this.update_counter[this.current_instruction_index]++
 		}
 		
-	} else {
+	}else{
 
 		this.no_out_counter++
 		
 		// write data to memory
-		this.memory[field_destination] = bit_data
+		if(this.memory[field_destination+this.current_instruction_index]!=bit_data){
+			this.memory[field_destination+this.current_instruction_index] = bit_data
+			this.update_counter[this.current_instruction_index]++
+		}
 	}
 
 	//  -------- path selection -------------
 	const path_selector_bit=this.memory[2]
-	if( path_selector_bit == 0 ) {
+	if(path_selector_bit==0){
 
 		const field_instruction_for_case_zero = current_instruction[2]
-		this.current_instruction_index = field_instruction_for_case_zero
+		this.current_instruction_index += field_instruction_for_case_zero
 		
-	} else if( path_selector_bit == 1 ) {
+	}else if(path_selector_bit==1){
 
 		const field_instruction_for_case_one = current_instruction[3]
-		this.current_instruction_index = field_instruction_for_case_one
+		this.current_instruction_index += field_instruction_for_case_one
 	}
 
 	const last_step=(correct_output && this.out_counter==out_expected_ref_len)||!correct_output||(this.no_out_counter>no_out_threshold)
@@ -139,3 +144,12 @@ execute_program(){
 m=new Machine()
 m.program=program
 m.execute_program()
+
+console.log('m.step_counter:'+m.step_counter)
+//console.log('m.program_array.length:'+m.program_array.length)
+console.log('m.update_counter:'+m.update_counter)
+console.log('value:'+m.update_counter.map(n=>n/m.step_counter))
+
+score=(m.step_counter)
+console.log('score:'+score)
+
